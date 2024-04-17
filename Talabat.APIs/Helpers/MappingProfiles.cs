8fa1bbb676d461f7d2dd1a99b89e28191cf762b0
Elattar11 <mohamed.ahmed7350@fmail.com>
2024-04-17 11:26:00 +0200

    Part 06 Product Picture Url Resolver

--- Talabat.APIs/Helpers/MappingProfiles.cs
+++ Talabat.APIs/Helpers/MappingProfiles.cs
@@ -6,13 +6,13 @@ namespace Talabat.APIs.Helpers
 {
 	public class MappingProfiles : Profile
 	{
-        public MappingProfiles()
+
+		public MappingProfiles()
         {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.Brand , O => O.MapFrom(s => s.Brand.Name))
-                .ForMember(d => d.Category , O => O.MapFrom(s => s.Category.Name));
-
-
-        }
+                .ForMember(d => d.Category , O => O.MapFrom(s => s.Category.Name))
+                .ForMember(P => P.PictureUrl , O => O.MapFrom<ProductsPictureUrlResolver>()); //Resolver 
+		}
     }
 }
---
+++ Talabat.APIs/Helpers/ProductsPictureUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using AutoMapper.Execution;
+using Talabat.APIs.DTOs;
+using Talabat.Core.Entities;
+
+namespace Talabat.APIs.Helpers
+{
+	public class ProductsPictureUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+	{
+		private readonly IConfiguration _conf;
+
+		public ProductsPictureUrlResolver(IConfiguration conf)
+        {
+			_conf = conf;
+		}
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+		{
+			if(!string.IsNullOrEmpty(source.PictureUrl))
+				return $"{_conf["ApiBaseUrl"]}/{source.PictureUrl}";
+
+			return string.Empty ;
+		}
+	}
+}
